dc13e5f97e50d0fcf0e584fb3315e618e65ce236
arir2001 R <amtrooney@gmail.com>
2024-08-18 19:06:02 +0000

    template tag directory and simple tag registered for most recent blog posts to be shown in other pages.

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,10 @@
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+@register.simple_tag
+
+@register.simple_tag
+def get_recent_blog_posts(count=1):
+    return Post.objects.filter(status=1).order_by('-created_on')[:count]
