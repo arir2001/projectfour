758310ce3d74797c87d9130a93e491e861525f4d
arir2001 R <amtrooney@gmail.com>
2024-08-19 09:50:59 +0000

    basic readme.md outlining models, apps.

--- blog/admin.py
+++ blog/admin.py
@@ -35,6 +35,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+    actions = [re_draft, publish, archive]
     
     
 
